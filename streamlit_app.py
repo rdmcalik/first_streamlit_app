77988eaf0170b1ce656c2b54090a63daeb7e9c1c
rdmcalik <131859950+rdmcalik@users.noreply.github.com>
2024-01-08 15:04:06 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,5 @@
 import streamlit
 
-streamlit.title('My Parents New Healty Dinner')
-
 streamlit.header ('Breakfast Favorites')
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
